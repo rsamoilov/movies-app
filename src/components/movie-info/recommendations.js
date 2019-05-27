c87a58de9d35baf057793f15be6a30e32917f02e
Roman Samoilov <rsamoilov@yandex.ua>
2019-05-27 17:25:40 +0300

    add recommendations header

--- src/components/movie-info/recommendations.js
+++ src/components/movie-info/recommendations.js
@@ -5,13 +5,17 @@ import configuration from "records/configuration";
 
 export default function Recommendations(props) {
   return (
-    <div className="d-flex justify-content-center">
-      {props.movies.slice(0, 5).map(movie => (
-        <div key={movie.id} className="col-2">
-          <img src={configuration.getImageUrl(movie.posterPath, 1)} className="img-fluid" />
-        </div>
-      ))}
-    </div>
+    <React.Fragment>
+      <h5 className="text-center">You may also like:</h5>
+
+      <div className="d-flex justify-content-center">
+        {props.movies.slice(0, 5).map(movie => (
+          <div key={movie.id} className="col-2">
+            <img src={configuration.getImageUrl(movie.posterPath, 1)} className="img-fluid" />
+          </div>
+        ))}
+      </div>
+    </React.Fragment>
   );
 }
 
