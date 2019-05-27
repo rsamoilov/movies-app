d0da289a50ad48dadfe612c1d30fc09ec1236c23
Roman Samoilov <rsamoilov@yandex.ua>
2019-05-27 17:48:26 +0300

    prevent propagation on favorite clicking; set cursor to pointer

--- src/components/favorite-button/index.js
+++ src/components/favorite-button/index.js
@@ -1,5 +1,6 @@
 import React, { useGlobal } from "reactn";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 import Octicon, { Star } from '@githubprimer/octicons-react';
 import FavoritesController from "controllers/favorites-controller";
 import MovieRecord from "records/movie-record";
@@ -10,13 +11,17 @@ export default function FavoriteButton({ movie }) {
   const [_, setCurrentFavoriteId] = useGlobal("favorite");
   const isFavorite = FavoritesController.isFavorite(movie);
 
-  function handleClick() {
+  function handleClick(e) {
+    e.preventDefault();
     FavoritesController.toggleFavorite(movie);
     setCurrentFavoriteId(movie.id);
   }
 
   return (
-    <div onClick={handleClick} className={isFavorite ? "FavoriteButton__selected" : ""}>
+    <div
+      onClick={handleClick}
+      className={classNames("FavoriteButton", { "FavoriteButton__selected": isFavorite })}
+    >
       <Octicon height={24}><Star /></Octicon>
     </div>
   );
