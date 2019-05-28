9fa2a605da0f40a2837307a7dd7f997d51015f6f
Roman Samoilov <rsamoilov@yandex.ua>
2019-05-28 16:27:45 +0300

    support pages in search

--- src/utils/location-search.js
+++ src/utils/location-search.js
@@ -1,17 +1,35 @@
 export default class LocationSearch {
   constructor(search) {
-    this.query = decodeURI(search.slice(3));
+    this.params = {}
+
+    search.replace("?", "").split("&").forEach(keyValue => {
+      const [key, value] = keyValue.split("=");
+
+      key === "page"  && this.setPage(value);
+      key === "query" && this.setQuery(value);
+    });
   }
 
   setQuery(query) {
-    this.query = query;
+    this.params.query = decodeURI(query);
   }
 
   getQuery() {
-    return this.query;
+    return this.params.query;
+  }
+
+  setPage(page) {
+    this.params.page = parseInt(page);
+  }
+
+  getPage() {
+    return this.params.page;
   }
 
   toString() {
-    return this.query ? `?q=${encodeURI(this.query)}` : "";
+    return Object.entries(this.params).
+      filter(([key, val]) => !!val).
+      map(keyVal => encodeURI(keyVal.join("="))).
+      join("&");
   }
 }
