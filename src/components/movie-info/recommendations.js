14f375cf0b9bf0ef950b61632ab66d117f10dbd2
Roman Samoilov <rsamoilov@yandex.ua>
2019-05-31 14:48:21 +0300

    preserve search query when following the recommendations

--- src/components/movie-info/recommendations.js
+++ src/components/movie-info/recommendations.js
@@ -2,8 +2,9 @@ import React from "reactn";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import MovieRecord from "records/movie-record";
+import withSearch from "components/hocs/with-search";
 
-export default function Recommendations(props) {
+function Recommendations(props) {
   if (!props.movies.length) {
     return null;
   }
@@ -15,7 +16,7 @@ export default function Recommendations(props) {
       <div className="d-flex justify-content-center flex-wrap">
         {props.movies.slice(0, 5).map(movie => (
           <div key={movie.id} className="col-4 col-md-2 mb-2 mb-md-0">
-            <Link to={`/movies/${movie.id}`}>
+            <Link to={{ pathname: `/movies/${movie.id}`, search: props.getSearch() }}>
               <img src={movie.getPosterURL(1)} className="img-fluid" />
             </Link>
           </div>
@@ -28,3 +29,5 @@ export default function Recommendations(props) {
 Recommendations.propTypes = {
   movies: PropTypes.arrayOf(MovieRecord)
 };
+
+export default withSearch(Recommendations);
